5d1f6e7395546a423da3dba5ca0e051518ef7375
clarencelol <clarencekuiek@proton.me>
2024-04-08 21:24:41 +0800

    Revert "gms: Adapt to new Gapps"
    This reverts commit 8559db167fd27b28ce2d0427bf112c027602d782.

--- config/common.mk
+++ config/common.mk
@@ -119,21 +119,10 @@ endif
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=log
 
-BLAZE_PIXEL ?= false
-BLAZE_PIXEL_6 ?= false
-BLAZE_MINI_GAPPS ?= false
-BLAZE_PICO_GAPPS ?= false
-
-
 # Gapps
 ifeq ($(WITH_GAPPS),true)
-ifeq ($(BLAZE_MINI_GAPPS),true)
-$(call inherit-product, vendor/gms/gms_mini.mk)
-else ifeq ($(BLAZE_PICO_GAPPS),true)
-$(call inherit-product, vendor/gms/gms_pico.mk)
-else
-$(call inherit-product, vendor/gms/gms_full.mk)
-endif
+$(call inherit-product-if-exists, vendor/gms/products/gms.mk)
+$(call inherit-product-if-exists, vendor/google/pixel/config.mk)
 endif
 
 # Google Photos Pixel Exclusive XML
